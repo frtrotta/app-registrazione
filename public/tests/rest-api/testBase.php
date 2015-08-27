32a83ae81f5d2f98beb3cb46c9055fd1e830b163
Francesco Trotta <fratrotta@gmail.com>
2015-08-27 09:37:58 +0200

    REST API v0.10 Implementate funzioni di login, logout, me

---
+++ public/tests/rest-api/testBase.php
@@ -0,0 +1,115 @@
+<?php
+
+//preg_match("/(\d{4})-(\d{2})-(\d{2})/", "2012-10-20", $results);
+//var_dump($results);
+
+class cUrlResult {
+
+    public $request;
+    public $response;
+
+    public function __construct() {
+        $this->request = new httpRequest();
+        $this->response = new httpResponse();
+    }
+
+}
+
+class httpRequest {
+
+    public $header;
+
+}
+
+class httpResponse {
+
+    public $code;
+    public $header;
+    public $contentType;
+    public $content;
+    public $cookies;
+
+}
+
+function http_request($url, $cookies = NULL) {
+    $options = array(
+        CURLOPT_RETURNTRANSFER => true, // return web page
+        CURLOPT_HEADER => true, // return headers
+        CURLINFO_HEADER_OUT => true,
+        CURLOPT_FOLLOWLOCATION => false, // follow redirects
+        CURLOPT_ENCODING => "", // handle compressed
+        CURLOPT_USERAGENT => "test", // name of client
+        CURLOPT_CONNECTTIMEOUT => 120, // time-out on connect
+        CURLOPT_TIMEOUT => 120, // time-out on response
+    );
+
+    $headers = [];
+    if (isset($cookies)) {
+        foreach ($cookies as $name => $value) {
+            array_push($headers, "Cookie: $name=$value");
+        }
+        $options[CURLOPT_HTTPHEADER] = $headers;
+    }
+
+    $ch = curl_init($url);
+    curl_setopt_array($ch, $options);
+    $r = new cUrlResult();
+    $r->response->content = curl_exec($ch);
+
+    $matches = null;
+    preg_match('![\n\r]{4}(.*)!mi', $r->response->content, $matches);
+    if (isset($matches[0])) {
+        $r->response->body = $matches[0];
+    }
+
+    $matches = null;
+    preg_match_all('!^Set-Cookie: (\w*)=(\w*)!mi', $r->response->content, $matches, PREG_SET_ORDER);
+
+    foreach ($matches as $m) {
+        if (isset($m[1]) && isset($m[2])) {
+            $r->response->cookies[$m[1]] = $m[2];
+        } else {
+            throw new Exception('No cookie matched');
+        }
+    }
+
+    $r->response->code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    $r->response->contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+    $r->request->header = curl_getinfo($ch, CURLINFO_HEADER_OUT);
+    curl_close($ch);
+
+    return $r;
+}
+
+function testPassed($testCode) {
+    echo "<tr class=\"success\">"
+    . "<td colspan=\"2\">Test&nbsp;$testCode&nbsp;OK</td>"
+    . "</tr>";
+}
+
+function testFailed($testCode, $cUrlResult) {
+    echo "<tr class=\"danger\">"
+    . "<td><strong>Test&nbsp;$testCode&nbsp;failed</strong></td>"
+    . "<td>"
+    . "<pre class=\"small\">" . $cUrlResult->request->header . "</pre>"
+    . "<pre class=\"small\">" . $cUrlResult->response->content . "</pre>"
+    . "</td>"
+    . "</tr>";
+}
+
+function testFailedMsg($testCode,  $cUrlResult, $msg) {
+    echo "<tr class=\"danger\">"
+    . "<td><strong>Test&nbsp;$testCode&nbsp;failed</strong></td>"
+    . "<td>"
+    . "<pre>" . $msg . "</pre>"
+    . "<pre class=\"small\">" . $cUrlResult->request->header . "</pre>"
+    . "<pre class=\"small\">" . $cUrlResult->response->content . "</pre>"
+    . "</td>"
+    . "</tr>";
+}
+
+$conf = parse_ini_file(__DIR__ . '\..\..\rest-api\config.ini', true);
+$mysqlConf = $conf['mysql'];
+$authConf = $conf['auth'];
+?>
+
