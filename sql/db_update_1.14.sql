908d236a47ebbd9575d0839357aa67e9d19a80ba
Francesco Trotta <fratrotta@gmail.com>
2015-09-10 16:48:31 +0200

    Fized db update script 1.14

--- sql/db_update_1.14.sql
+++ sql/db_update_1.14.sql
@@ -2,6 +2,14 @@ USE `aisla_triathlon`;
 
 ALTER TABLE `utente`
 	DROP username;
+	
+UPDATE `utente`
+	SET email = '[da richiedere]'
+	WHERE nome = '' AND cognome = '';
+	
+UPDATE `utente`
+	SET email = '[da richiedere 1]'
+	WHERE nome = 'Franco' AND cognome = 'Rastelli';
 
 ALTER TABLE `utente`
   ADD UNIQUE KEY `email` (`email`);
